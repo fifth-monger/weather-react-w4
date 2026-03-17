90c80e05130cdd00d29b41ab5d8f72dbdf708a78
Vi <107654718+fifth-monger@users.noreply.github.com>
2026-03-17 15:37:07 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -6,26 +6,28 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <Weather />
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://victoriagarcia.netlify.app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Victoria Garcia
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/fifth-monger/weather-react-w4"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://victoriagarcia.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Victoria Garcia
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/fifth-monger/weather-react-w4"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
