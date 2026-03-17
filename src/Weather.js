3dff7032aa14120351f0e0de52443e8f998a46a9
Vi <107654718+fifth-monger@users.noreply.github.com>
2026-03-17 14:28:31 -0500

    removed Threedots

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { ThreeDots } from "react-loader-spinner";
 
 export default function Weather() {
   const [city, setCity] = useState("");
