5e242208a3fa5c9a65295af0bc0e727d6d440ca7
Vi <107654718+fifth-monger@users.noreply.github.com>
2026-03-17 14:12:21 -0500

    made minor design tweeks and moved project from codesandbox to VS

--- src/App.js
+++ src/App.js
@@ -6,10 +6,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hola Mundo</h1>
-        <Weather city="Odem" />
-      </header>
+      <h1>Weather App</h1>
+      <Weather />
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,26 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { ThreeDots } from "react-loader-spinner";
 
-export default function Weather(props) {
-  function handleResponse(response) {
-    alert(
-      `The weather in ${response.data.city} is ${Math.round(response.data.temperature.current)}°F`,
-    );
+export default function Weather() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false); //going to tell us if we have results
+  const [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      location: response.data.city,
+      temperature: response.data.temperature.current,
+      wind: response.data.wind.speed,
+      humidity: response.data.temperature.humidity,
+      icon: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`,
+      description: response.data.condition.description,
+    });
   }
-  let apiKey = "23ab4btd750623bd6a0096bbc3of4f4f";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.city}&key=${apiKey}&units=imperial`;
 
-  axios.get(apiUrl).then(handleResponse);
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "23ab4btd750623bd6a0096bbc3of4f4f";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
 
-  return (
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" placeholder="Enter a city.." onChange={updateCity} />
+      <button type="Submit">Search</button>
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+          <li>{weather.location}</li>
+          <li>Description: {weather.description}</li>
+          <li>Temperature: {Math.round(weather.temperature)}°F</li>
+          <li>Wind: {Math.round(weather.wind)}m/h</li>
+          <li>Humidity: {weather.humidity}%</li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
     <ThreeDots
       height="80"
       width="80"
       color="orange"
       ariaLabel="tail-spin-loading"
       visible={true}
-    />
-    // <div><h2>Hello from Weather</h2></div>
-  );
+    />;
+  }
 }
