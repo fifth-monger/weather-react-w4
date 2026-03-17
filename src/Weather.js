7acb77fba41e623b794b84958d346ed8c20a3278
Vi <107654718+fifth-monger@users.noreply.github.com>
2026-03-17 16:01:26 -0500

    started adding HTML

--- src/Weather.js
+++ src/Weather.js
@@ -1,57 +1,46 @@
 import React, { useState } from "react";
 import axios from "axios";
 
+import "./Weather.css";
+
 export default function Weather() {
-  const [city, setCity] = useState("");
-  const [loaded, setLoaded] = useState(false); //going to tell us if we have results
-  const [weather, setWeather] = useState({});
-
-  function displayWeather(response) {
-    setLoaded(true);
-    setWeather({
-      location: response.data.city,
-      temperature: response.data.temperature.current,
-      wind: response.data.wind.speed,
-      humidity: response.data.temperature.humidity,
-      icon: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`,
-      description: response.data.condition.description,
-    });
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    let apiKey = "23ab4btd750623bd6a0096bbc3of4f4f";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
-    axios.get(apiUrl).then(displayWeather);
-  }
-  function updateCity(event) {
-    setCity(event.target.value);
-  }
-
-  let form = (
-    <form onSubmit={handleSubmit}>
-      <input type="search" placeholder="Enter a city.." onChange={updateCity} />
-      <button type="Submit">Search</button>
-    </form>
-  );
-
-  if (loaded) {
-    return (
-      <div>
-        {form}
-        <ul>
-          <li>
-            <img src={weather.icon} alt={weather.description} />
-          </li>
-          <li>{weather.location}</li>
-          <li>Description: {weather.description}</li>
-          <li>Temperature: {Math.round(weather.temperature)}°F</li>
-          <li>Wind: {Math.round(weather.wind)}m/h</li>
-          <li>Humidity: {weather.humidity}%</li>
-        </ul>
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-dark" />
+          </div>
+        </div>
+      </form>
+      <h1>Oaxaca City</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://www.gstatic.com/weather/conditions/v1/svg/partly_cloudy_light.svg"
+            alt="partly cloudy"
+          />
+          71°F
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Percipitation: 15%</li>
+            <li>Humidity: 72%</li>
+            <li>Wind 13 km/h</li>
+          </ul>
+        </div>
       </div>
-    );
-  } else {
-    return form;
-  }
+    </div>
+  );
 }
