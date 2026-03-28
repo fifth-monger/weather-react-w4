e434e73e48d7fef721744a1f2e40a7e9524fc106
Victoria <victoriaeg369@gmail.com>
2026-03-28 17:04:02 -0500

    added bootstrap utility class to second column in weather info

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -27,7 +27,7 @@ export default function WeatherInfo(props) {
             </div>
           </div>
         </div>
-        <div className="col-6">
+        <div className="col-6 d-flex justify-content-end">
           <ul>
             <li>
               Feels Like:{" "}
